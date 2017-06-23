85e9989dad0dffcc71ca2cde4e604423de495fd7
Oscar González <oscarg117@gmail.com>
2017-06-22 21:15:48 -0600

    inicio Ejercicio 2, Experimento 4.

---
+++ Experimento04/Ejercicio02/Collaterals.v
@@ -0,0 +1,105 @@
+`timescale 1ns / 1ps
+`include "Defintions.v"
+
+//------------------------------------------------
+module UPCOUNTER_POSEDGE # (parameter SIZE = 16)
+       (
+         input wire Clock, Reset,
+         input wire [SIZE-1:0] Initial,
+         input wire Enable,
+         output reg [SIZE-1:0] Q
+       );
+
+
+always @(posedge Clock )
+  begin
+    if (Reset)
+      Q = Initial;
+    else
+      begin
+        if (Enable)
+          Q = Q + 1;
+
+      end
+  end
+
+endmodule
+  //----------------------------------------------------
+  module FFD_POSEDGE_SYNCRONOUS_RESET # ( parameter SIZE = 8 )
+  (
+    input wire	Clock,
+    input wire	Reset,
+    input wire	Enable,
+    input wire [SIZE-1:0]	D,
+    output reg [SIZE-1:0]	Q
+  );
+
+
+always @ (posedge Clock)
+  begin
+    if ( Reset )
+      Q <= 0;
+    else
+      begin
+        if (Enable)
+          Q <= D;
+      end
+
+  end //always
+
+endmodule
+
+
+  //----------------------------------------------------------------------
+
+
+  module VGA_controller
+  (
+    input wire clk25MHz,
+    input wire Reset,
+    input wire	[2:0]	iFromVRAM_RGB,
+    output wire	[2:0]	oToVGA_RGB,
+    output wire	oHSync,
+    output wire	oVSync,
+    output wire [9:0]	oVcnt,
+    output wire [9:0]	oHcnt
+  );
+wire wFromVRAM_R, wFromVRAM_G, wFromVRAM_B;
+wire wToVGA_R, wToVGA_G, wToVGA_B;
+wire wEOL;
+
+assign wFromVRAM_R = iFromVRAM_RGB[2];
+assign wFromVRAM_G = iFromVRAM_RGB[1];
+assign wFromVRAM_B = iFromVRAM_RGB[0];
+assign oToVGA_RGB = {wToVGA_R, wToVGA_G, wToVGA_B};
+
+assign oHSync = (oHcnt < `HS_Ts - `HS_Tpw) ? 1'b1 : 1'b0;
+assign wEOL = (oHcnt == `HS_Ts - 1);
+assign oVSync = (oVcnt < `VS_lines_Ts - 1) ? 1'b1 : 1'b0;
+
+assign {wToVGA_R, wToVGA_G, wToVGA_B} = ( oVcnt < `VS_lines_Tbp+`V_OFFSET  ||
+                                    oVcnt >= `VS_lines_Ts-`VS_lines_Tpw-`VS_lines_Tfp-`V_OFFSET ||
+                                    oHcnt < `HS_Tbp+`H_OFFSET ||
+                                    oHcnt > `HS_Ts-`HS_Tpw-`HS_Tfp-`H_OFFSET  )
+                                    ? `BLACK : {wFromVRAM_R, wFromVRAM_G, wFromVRAM_B};
+
+UPCOUNTER_POSEDGE # (10) HORIZONTAL_COUNTER
+                  (
+                    .Clock	( clk25MHz ),
+                    .Reset	( (oHcnt > `HS_Ts-1) || Reset ),
+                    .Initial	( 10'b0 ),
+                    .Enable	( 1'b1	),
+                    .Q	(	oHcnt )
+                  );
+
+UPCOUNTER_POSEDGE # (10) VERTICAL_COUNTER
+                  (
+                    .Clock	( clk25MHz ),
+                    .Reset	( (oVcnt > `VS_lines_Ts-1) || Reset ),
+                    .Initial	( 10'b0 ),
+                    .Enable	( wEOL ),
+                    .Q	( oVcnt )
+                  );
+
+endmodule
+  //----------------------------------------------------------------------
---
+++ Experimento04/Ejercicio02/MiniAlu.v
@@ -0,0 +1,308 @@
+`timescale 1ns / 1ps
+`include "Defintions.v"
+
+module MiniAlu
+       (
+         input  wire Clock,
+         input  wire Reset,
+         output wire VGA_RED, VGA_GREEN, VGA_BLUE,
+         output wire VGA_HSYNC,
+         output wire VGA_VSYNC
+       );
+
+wire [15:0] wIP, wIP_temp;
+wire [27:0] wInstruction;
+wire [3: 0] wOperation;
+wire [7: 0] wSourceAddr0, wSourceAddr1, wDestination;
+wire [15:0] wSourceData0, wSourceData1, wIPInitialValue, wImmediateValue;
+
+reg rWriteEnable, rBranchTaken;
+reg [15:0] rResult, rResultHI;
+reg rVGAWriteEnable;
+reg rRetCall;
+reg [7: 0] rRetIP;
+
+wire [7:0] wRetCall;
+wire wVGA_R, wVGA_G, wVGA_B;
+wire [9:0] wHcnt, wVcnt;
+wire [9:0] wCurrCol, wCurrRow;
+
+assign wCurrCol = ( wHcnt >= `HS_Tbp &&
+                   wHcnt < `HS_Tdisp + `HS_Tbp) ?
+       (wHcnt - `HS_Tbp) : 10'd0;
+
+assign wCurrRow = ( wVcnt >= `VS_lines_Tbp &&
+                   wVcnt < `VS_lines_Tdisp + `VS_lines_Tbp) ?
+       (wVcnt - `VS_lines_Tbp) : 10'd0;
+
+wire Clock25MHz;
+UPCOUNTER_POSEDGE # ( 1 ) HalfCLK
+                  (
+                    .Clock( Clock ),
+                    .Reset( Reset),
+                    .Initial( 1'd0 ),
+                    .Enable( 1'b1 ),
+                    .Q( Clock25MHz )
+                  );
+
+
+
+VGA_controller VGA_ctrl
+               (
+                 .clk25MHz( Clock25MHz ),
+                 .Reset( Reset ),
+                 .iFromVRAM_RGB( {wVGA_R, wVGA_G, wVGA_B} ),
+                 .oToVGA_RGB( {VGA_RED, VGA_GREEN, VGA_BLUE} ),
+                 .oHSync( VGA_HSYNC ),
+                 .oVSync( VGA_VSYNC ),
+                 .oVcnt( wVcnt ),
+                 .oHcnt( wHcnt )
+               );
+
+
+
+ROM InstructionRom
+    (
+      .iAddress( wIP ),
+      .oInstruction( wInstruction )
+    );
+
+
+RAM_SINGLE_READ_PORT # (3, 13, 80*60) VideoRAM
+                     (
+                       .Clock(Clock),
+                       .iWriteEnable( rVGAWriteEnable ),
+                       .iReadAddress( {wCurrCol[9:3], wCurrRow[8:3]} ),  // Col, Row
+                       .iWriteAddress( {wSourceData1[6:0], wSourceData0[5:0]} ),  // Col, Row
+                       .iDataIn( wDestination[2:0] ),
+                       .oDataOut( {wVGA_R, wVGA_G, wVGA_B} )
+                     );
+
+
+RAM_DUAL_READ_PORT # (16, 8, 16) DataRam
+                   (
+                     .Clock( Clock ),
+                     .iWriteEnable( rWriteEnable ),
+                     .iReadAddress0( wInstruction[7: 0] ),
+                     .iReadAddress1( wInstruction[15:8] ),
+                     .iWriteAddress( wDestination ),
+                     .iDataIn( rResult ),
+                     .oDataOut0( wSourceData0 ),
+                     .oDataOut1( wSourceData1 )
+                   );
+
+always @ (posedge Clock)
+  begin
+    if (wOperation == `CALL)
+      rRetIP <= wIP_temp;
+  end
+
+assign wImmediateValue = {wSourceAddr1, wSourceAddr0};
+
+assign wIP = (rBranchTaken) ? wIPInitialValue : wIP_temp;
+assign wIPInitialValue = (Reset) ? 8'b0 : wRetCall;
+assign wRetCall = (rRetCall) ? rRetIP : wDestination;
+
+UPCOUNTER_POSEDGE IP
+                  (
+                    .Clock( Clock ),
+                    .Reset( Reset | rBranchTaken ),
+                    .Initial( wIPInitialValue + 16'd1 ),
+                    .Enable( 1'b1 ),
+                    .Q( wIP_temp )
+                  );
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 4 ) FFD1
+                             (                //Operacion
+                               .Clock(Clock),
+                               .Reset(Reset),
+                               .Enable(1'b1),
+                               .D(wInstruction[27: 24]),
+                               .Q(wOperation)
+                             );
+
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 8 ) FFD2
+                             (                //Registro 0 (Der)
+                               .Clock(Clock),
+                               .Reset(Reset),
+                               .Enable(1'b1),
+                               .D(wInstruction[7: 0]),
+                               .Q(wSourceAddr0)
+                             );
+
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 8 ) FFD3
+                             (                //Registro 1 (Centro)
+                               .Clock(Clock),
+                               .Reset(Reset),
+                               .Enable(1'b1),
+                               .D(wInstruction[15: 8]),
+                               .Q(wSourceAddr1)
+                             );
+
+FFD_POSEDGE_SYNCRONOUS_RESET # ( 8 ) FFD4
+                             (                //Registro destino (Izq)
+                               .Clock(Clock),
+                               .Reset(Reset),
+                               .Enable(1'b1),
+                               .D(wInstruction[23: 16]),
+                               .Q(wDestination)
+                             );
+
+always @ ( * )
+  begin
+    case (wOperation)
+      //-------------------------------------
+      `NOP:
+        begin
+          rBranchTaken <= 1'b0;
+          rWriteEnable <= 1'b0;
+          {rResultHI, rResult} <= 32'd0;
+          rVGAWriteEnable <= 1'b0;
+          rRetCall <= 1'b0;
+        end
+
+      //-------------------------------------
+      `ADD:
+        begin
+          rBranchTaken <= 1'b0;
+          rWriteEnable <= 1'b1;
+          {rResultHI, rResult} <= wSourceData1 + wSourceData0;
+          rVGAWriteEnable <= 1'b0;
+          rRetCall <= 1'b0;
+        end
+
+      //-------------------------------------
+      `SUB:
+        begin
+          rBranchTaken <= 1'b0;
+          rWriteEnable <= 1'b1;
+          {rResultHI, rResult} <= wSourceData1 - wSourceData0;
+          rVGAWriteEnable <= 1'b0;
+          rRetCall <= 1'b0;
+        end
+
+      //-------------------------------------
+      `STO:
+        begin
+          rWriteEnable <= 1'b1;
+          rBranchTaken <= 1'b0;
+          {rResultHI, rResult} <= wImmediateValue;
+          rVGAWriteEnable <= 1'b0;
+          rRetCall <= 1'b0;
+        end
+
+      //-------------------------------------
+      `BLE:
+        begin
+          rWriteEnable <= 1'b0;
+          {rResultHI, rResult} <= 32'd0;
+          rVGAWriteEnable <= 1'b0;
+          rRetCall <= 1'b0;
+
+          if (wSourceData1 <= wSourceData0 )
+            rBranchTaken <= 1'b1;
+          else
+            rBranchTaken <= 1'b0;
+
+        end
+
+      //-------------------------------------
+      `BGE:
+        begin
+          rWriteEnable <= 1'b0;
+          {rResultHI, rResult} <= 32'd0;
+          rVGAWriteEnable <= 1'b0;
+          rRetCall <= 1'b0;
+
+          if (wSourceData1 >= wSourceData0 )
+            rBranchTaken <= 1'b1;
+          else
+            rBranchTaken <= 1'b0;
+        end
+
+      //-------------------------------------
+      `JMP:
+        begin
+          rWriteEnable <= 1'b0;
+          {rResultHI, rResult} <= 32'd0;
+          rBranchTaken <= 1'b1;
+          rVGAWriteEnable <= 1'b0;
+          rRetCall <= 1'b0;
+        end
+
+      //-------------------------------------
+      //-------------------------------------
+      `SMUL:
+        begin
+          rBranchTaken <= 1'b0;
+          rWriteEnable <= 1'b1;
+          {rResultHI, rResult} <= wSourceData1 * wSourceData0;
+          rVGAWriteEnable <= 1'b0;
+          rRetCall <= 1'b0;
+        end
+
+      //--------------------------------------
+      `CALL:
+        begin
+          rWriteEnable <= 1'b0;
+          {rResultHI, rResult} <= 32'd0;
+          rBranchTaken <= 1'b1;
+          rVGAWriteEnable <= 1'b0;
+          rRetCall <= 1'b0;
+        end
+
+      //--------------------------------------
+      `RET:
+        begin
+          rWriteEnable <= 1'b0;
+          {rResultHI, rResult} <= 32'd0;
+          rBranchTaken <= 1'b1;
+          rVGAWriteEnable <= 1'b0;
+          rRetCall <= 1'b1;
+        end
+
+      //-------------------------------------
+      `VGA:
+        begin
+          rWriteEnable <= 1'b0;
+          rBranchTaken <= 1'b0;
+          {rResultHI, rResult} <= 32'd0;
+          rVGAWriteEnable <= 1'b1;
+          rRetCall <= 1'b0;
+        end
+
+      //-------------------------------------
+      `INC:
+        begin
+          rBranchTaken <= 1'b0;
+          rWriteEnable <= 1'b1;
+          {rResultHI, rResult} <= wSourceData1 + 1;
+          rVGAWriteEnable <= 1'b0;
+          rRetCall <= 1'b0;
+        end
+
+      //-------------------------------------
+      `MOV:
+        begin
+          rBranchTaken <= 1'b0;
+          rWriteEnable <= 1'b1;
+          {rResultHI, rResult} <= wSourceData1;
+          rVGAWriteEnable <= 1'b0;
+          rRetCall <= 1'b0;
+        end
+
+      //-------------------------------------
+      default:
+        begin
+          rWriteEnable <= 1'b0;
+          {rResultHI, rResult} <= 32'd0;
+          rBranchTaken <= 1'b0;
+          rVGAWriteEnable <= 1'b0;
+          rRetCall <= 1'b0;
+        end
+
+      //-------------------------------------
+    endcase
+  end
+
+
+endmodule
